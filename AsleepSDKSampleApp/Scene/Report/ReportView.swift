8356c37f17f68b4b974064e27d79af4bb3aac602
eddie-lee101 <eddie.lee@asleep.ai>
2023-10-04 14:52:21 +0900

    Asleep SDK iOS Sample app for SDK 1.x.x

--- AsleepSDKSampleApp/Scene/Report/ReportView.swift
+++ AsleepSDKSampleApp/Scene/Report/ReportView.swift
@@ -11,7 +11,7 @@ struct ReportView: View {
             HStack {
                 Spacer()
                 Button {
-                    presentationMode.wrappedValue.dismiss()	
+                    presentationMode.wrappedValue.dismiss()
                 } label: {
                     Image(systemName: "xmark.circle.fill")
                 }
@@ -33,8 +33,7 @@ struct ReportView: View {
                 .font(.title2.bold())
             Text("Time Range: \(report.session.startTime.dateString + (report.session.endTime.map { " ~ " + $0.dateString } ?? ""))")
             Text("Session State: \(report.session.state.rawValue)")
-            Text("Missing Data Ratio: \(String(report.missingDataRatio))")
-            Text("Preculiarities: \(report.peculiarities.description)")
+            Text("Validity: \(report.validity.rawValue)")
             stagesView(report: report)
         }
         .padding(.horizontal)
@@ -44,25 +43,61 @@ struct ReportView: View {
     func stagesView(report: Asleep.Model.Report) -> some View {
         
         VStack(alignment: .leading, spacing: 10) {
-            Text("Stat")
-                .font(.body.bold())
-                .padding(.top, 4)
-            Text("\(report.stat?.description ?? "null")")
+            statView(stat: report.stat)
             
             Text("Sleep Stages")
                 .font(.body.bold())
                 .padding(.top, 4)
-            if let sleepStages = report.session.sleepStages {
-                Text("[\(sleepStages.map(String.init).joined(separator: ", "))]")
-            }
+            let sleepStages = report.session.sleepStages
+            Text("[\(sleepStages.map{ "\($0.rawValue)" }.joined(separator: ", "))]")
             
             Text("Breath Stages")
                 .font(.body.bold())
                 .padding(.top, 4)
-            if let breathStages = report.session.breathStages {
-                Text("[\(breathStages.map(String.init).joined(separator: ", "))]")
-            }
+
+            let breathStages = report.session.osaStages
+            Text("[\(breathStages.map{ "\($0.rawValue)" }.joined(separator: ", "))]")
         }
+    }
+    
+    @ViewBuilder
+    func statView(stat: Asleep.Model.Stat?) -> some View {
+        VStack(alignment: .leading) {
+            Text("Stat")
+                .font(.body.bold())
+                .padding(.vertical, 4)
+
+            Text("SleepLatency: \((stat?.sleepLatency).string)")
+            Text("WakeupLatency: \((stat?.wakeupLatency).string)")
+            Text("SleepTime: \((stat?.sleepTime).string)")
+            Text("WakeTime: \((stat?.wakeTime).string)")
+            Text("TimeInWake: \((stat?.timeInWake).string)")
+            Text("TimeInSleep: \((stat?.timeInSleep).string)")
+            Text("TimeInBed: \((stat?.timeInBed).string)")
+            Text("TimeInSleepPeriod: \((stat?.timeInSleepPeriod).string)")
+            Text("TimeInREM: \((stat?.timeInRem).string)")
+            Text("TimeInLight: \((stat?.timeInLight).string)")
+            Text("TimeInDeep: \((stat?.timeInDeep).string)")
+            Text("TimeInDeep: \((stat?.timeInDeep).string)")
+            Text("TimeInStableBreath: \((stat?.timeInStableBreath).string)")
+            Text("TimeInUnstableBreath: \((stat?.timeInUnstableBreath).string)")
+            Text("SleepEfficiency: \((stat?.sleepEfficiency).string)")
+            Text("WakeRatio: \((stat?.wakeRatio).string)")
+            Text("SleepRatio: \((stat?.sleepRatio).string)")
+            Text("RemRatio: \((stat?.remRatio).string)")
+            Text("LightRatio: \((stat?.lightRatio).string)")
+            Text("DeepRatio: \((stat?.deepRatio).string)")
+            Text("StableBreathRatio: \((stat?.stableBreathRatio).string)")
+            Text("UnstableBreathRatio: \((stat?.unstableBreathRatio).string)")
+            Text("BreathingPattern: \((stat?.breathingPattern).string)")
+            Text("EstimatedAhi: \((stat?.estimatedAhi).string)")
+        }
+    }
+}
+
+private extension Optional {
+    var string: String {
+        self.map{ "\($0)" } ?? "N/A"
     }
 }
 
@@ -71,42 +106,45 @@ struct ReportView_Previews: PreviewProvider {
         let dummyReport: Asleep.Model.Report?
         let jsonData = """
         {
-            "timezone": "Asia/Seoul",
-            "peculiarities": [],
-            "missingDataRatio": 0.0,
-            "session": {
-                "id": "20230914124256_8ramp",
-                "state": "COMPLETE",
-                "startTime": "2023-09-14T21:42:56+09:00",
-                "endTime":"2023-09-14T22:08:34+09:00",
-                "sleepStages": [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3],
-                "breathStages": [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
-            },
-            "stat": {
-                "sleepEfficiency": 0.8,
-                "sleepLatency": 300,
-                "sleepTime": "2023-09-14T21:47:56+09:00",
-                "wakeupLatency": 0,
-                "wakeTime": "2023-09-14T22:08:34+09:00",
-                "timeInWake": 0,
-                "timeInSleep": 1200,
-                "timeInBed": 1538,
-                "timeInSleepPeriod": 1200,
-                "timeInRem": 300,
-                "timeInLight": 900,
-                "timeInDeep": 0,
-                "timeInStableBreath": 1200,
-                "timeInUnstableBreath": 0,
-                "wakeRatio": 1,
-                "sleepRatio": 1,
-                "remRatio": 0.25,
-                "lightRatio": 0.75,
-                "deepRatio": 0,
-                "stableBreathRatio": 1,
-                "unstableBreathRatio": 0,
-                "breathingPattern": "STABLE_BREATH",
-                "breathingIndex": 0
-            }
+          "timezone" : "Asia/Seoul",
+          "validity" : "VALID",
+          "session" : {
+            "osaStages" : [
+              0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0
+            ],
+            "id" : "20231004040255_zsv1s",
+            "state" : "COMPLETE",
+            "startTime" : "2023-10-04T13:02:55+09:00",
+            "endTime" : "2023-10-04T13:23:41+09:00",
+            "sleepStages" : [
+              0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,1 ,1 ,1 ,1 ,1 ,1 ,1 ,1 ,1 ,1 ,1 ,1 ,1 ,1 ,1 ,1 ,1 ,1 ,1 ,1 ,1 ,1 ,1 ,1 ,1 ,1 ,1 ,1 ,1 ,1
+            ]
+          },
+          "stat" : {
+            "timeInWake" : 0,
+            "timeInSleepPeriod" : 900,
+            "timeInDeep" : 0,
+            "lightRatio" : 1,
+            "timeInRem" : 0,
+            "estimatedAhi" : 0,
+            "remRatio" : 0,
+            "wakeupLatency" : 0,
+            "wakeTime" : "2023-10-04T13:23:41+09:00",
+            "stableBreathRatio" : 1,
+            "sleepRatio" : 1,
+            "wakeRatio" : 0,
+            "timeInStableBreath" : 900,
+            "sleepLatency" : 300,
+            "timeInBed" : 1246,
+            "deepRatio" : 0,
+            "timeInLight" : 900,
+            "timeInUnstableBreath" : 0,
+            "unstableBreathRatio" : 0,
+            "breathingPattern" : "VERY_STABLE",
+            "timeInSleep" : 900,
+            "sleepEfficiency" : 0.75,
+            "sleepTime" : "2023-10-04T13:07:55+09:00"
+          }
         }
         """.data(using: .utf8)!
         let decoder = JSONDecoder()
